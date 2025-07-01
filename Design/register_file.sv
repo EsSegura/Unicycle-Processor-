6cd4ad21884bfae998a4f2772f2c1d3ef70c6832
EsSegura <segurae28@gmail.com>
2025-07-01 15:16:21 -0600

    a

--- Design/register_file.sv
+++ Design/register_file.sv
@@ -16,30 +16,32 @@ module reg_file #(parameter WIDTH = 32, parameter DEPTH = 5)(
 
     logic [WIDTH-1:0] registers [DEPTH];
     logic [WIDTH-1:0] reg_zero;
-    logic [DEPTH-1:0] i;
+
 
     assign reg_zero = 0;
 
-    always_ff @( posedge clk ) begin 
-        if (rst) begin 
-            for (int i=1; i<DEPTH; i++)begin 
-                registers[i] <= '0;
-            end
-        end
-        else begin 
-            if ((write) && (write_register != 0))
-                registers [write_register] <= write_data;
-            if(read) begin 
-                if (read_register_1 == 0)
-                    read_data_1 <= reg_zero;
-                else 
-                    read_data_1 <= registers[read_register_1];
-                if (read_register_2 == 0)
-                    read_data_2 <= reg_zero;
-                else 
-                    read_data_2 <= registers[read_register_2];  
-            end
+    genvar i;
+    generate
+    for (i = 0; i < DEPTH; i++) begin
+        always_ff @(posedge clk) begin
+                if (rst) begin
+                    registers[i] <= '0;
+                end else begin 
+                    if ((write) && (write_register != 0))
+                        registers [write_register] <= write_data;
+                    if(read) begin 
+                        if (read_register_1 == 0)
+                            read_data_1 <= reg_zero;
+                        else 
+                            read_data_1 <= registers[read_register_1];
+                        if (read_register_2 == 0)
+                            read_data_2 <= reg_zero;
+                        else 
+                            read_data_2 <= registers[read_register_2];
+                    end
+                end
         end
     end
+    endgenerate
 
 endmodule
